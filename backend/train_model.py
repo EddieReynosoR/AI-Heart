0bbe83cfd9abdf3746b22bfe8cdf259e61fb552c
EddieReynosoR <eduardo_3reynoso@hotmail.com>
2025-05-19 21:33:06 -0700

    Mejoras en modelo

--- backend/train_model.py
+++ backend/train_model.py
@@ -4,9 +4,12 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import joblib
+from sklearn.utils import class_weight
+from sklearn.metrics import roc_curve, auc
+import numpy as np
 
 # Cargar datos
-df = pd.read_csv("heart.csv")
+df = pd.read_csv("heart_smote.csv")
 X = df.drop("target", axis=1)
 y = df["target"]
 
@@ -18,22 +21,34 @@ joblib.dump(scaler, "scaler.pkl")  # Guardar escalador
 # Dividir en sets de entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
+weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+class_weights = dict(enumerate(weights))
+
 # Definir modelo
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(16, activation="relu", input_shape=(X_train.shape[1],),
-                          kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(64, activation="relu", input_shape=(X_train.shape[1],)),
+    tf.keras.layers.BatchNormalization(),
+    
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    
+    tf.keras.layers.Dense(16, activation="relu"),
+    tf.keras.layers.BatchNormalization(),
+    
     tf.keras.layers.Dense(1, activation="sigmoid")
 ])
 
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
+              loss="binary_crossentropy", 
+              metrics=["accuracy"])
 
 # Entrenar modelo
 history = model.fit(
     X_train, y_train,
-    epochs=500,
+    epochs=150,
     validation_data=(X_test, y_test),
-    batch_size=16,
+    batch_size=32,
+    class_weight=class_weights,
     verbose=1
 )
 
@@ -57,4 +72,20 @@ plt.ylabel("Loss")
 plt.legend()
 plt.title("Model Loss")
 plt.savefig("loss_plot.png")
+plt.close()
+
+y_pred_prob = model.predict(X_test).ravel()
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, color="darkorange", lw=2, label=f"ROC curve (AUC = {roc_auc:.2f})")
+plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver Operating Characteristic")
+plt.legend(loc="lower right")
+plt.savefig("roc_auc_plot.png")
 plt.close()
